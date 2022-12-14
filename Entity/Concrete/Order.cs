5f3b2e196dc52d9c60c01dd7a4048ebe4864d884
Batuhan <91565731+BizonTR@users.noreply.github.com>
2022-12-15 00:32:48 +0300

    edit

---
+++ Entity/Concrete/Order.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Order: IEntity
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
---
+++ Entity/Concrete/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class OrderDetail : IEntity
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
