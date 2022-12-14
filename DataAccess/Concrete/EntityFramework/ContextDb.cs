4220a0479d2477e39fb4c8c5e52dbe10926e8c7c
slimkiddjudas <umutcerk@gmail.com>
2022-12-14 20:12:41 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/ContextDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class ContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=.\sqlexpress ; database=BackEndProject ; trusted_connection=true ; TrustServerCertificate = true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Entity/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entity.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public string Description { get; set; }
+    }
+}
