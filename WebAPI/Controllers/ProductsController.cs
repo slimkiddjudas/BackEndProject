03a5415b696593e25ef6f8a46e18d190e4ea9c33
slimkiddjudas <umutcerk@gmail.com>
2022-12-15 16:14:52 +0300

    Added API

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _productService.GetAll();
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest();
+        }
+    }
+}
