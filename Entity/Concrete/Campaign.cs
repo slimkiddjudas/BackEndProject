3dee204f079ffaab8beb730f96ffcf89714c87d6
Batuhan <91565731+BizonTR@users.noreply.github.com>
2022-12-15 17:12:12 +0300

    Update

---
+++ Entity/Concrete/Campaign.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Campaign : IEntity
+    {
+        public int CampaignId { get; set; }
+        public int ProductId { get; set; }
+        public int Discount { get; set; }
+        public string Description { get; set; }
+    }
+}
