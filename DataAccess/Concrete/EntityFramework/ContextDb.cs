907b7bbc67f22b2478be7c50c8a13ce4f5878463
slimkiddjudas <umutcerk@gmail.com>
2022-12-15 16:29:11 +0300

    Update

--- DataAccess/Concrete/EntityFramework/ContextDb.cs
+++ DataAccess/Concrete/EntityFramework/ContextDb.cs
@@ -16,5 +16,9 @@ namespace DataAccess.Concrete.EntityFramework
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
