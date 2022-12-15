b0b5a34f65744d5f59b9ccfb26e726725e3eee4b
Batuhan <91565731+BizonTR@users.noreply.github.com>
2022-12-15 16:41:17 +0300

    Update

---
+++ Entity/Concrete/CartItems.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class CartItems : IEntity
+    {
+        public int CartItemId { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
